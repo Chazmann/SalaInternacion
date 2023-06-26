98c2234c35b37f9547e67aefe96b679ec58bc069
Leandro Chazarreta <lchaz85@gmail.com>
2023-06-26 12:58:57 -0300

    Indent

--- js/listar.js
+++ js/listar.js
@@ -39,25 +39,25 @@ function tarJeta() {
                 camas.innerHTML += ` 
                 
                 <div class="cama ">
-            <div class="head ${crearsex(persona.sexo)} flex_x"><i class="material-icons-outlined rojo">${seguimiento(persona.active)}</i>CAMA-A002-${'0'+persona.cama}</div>
-            <div class="paciente flex_x fcol">
-                <div class="datos w100 flex_x">
-                    <h5>${persona.name}</h5>
-                    <h5>${persona.edad+' años'}</h5>
+                    <div class="head ${crearsex(persona.sexo)} flex_x"><i class="material-icons-outlined rojo">${seguimiento(persona.active)}</i>CAMA-A002-${'0'+persona.cama}</div>
+                    <div class="paciente flex_x fcol">
+                         <div class="datos w100 flex_x">
+                             <h5>${persona.name}</h5>
+                             <h5>${persona.edad+' años'}</h5>
+                         </div>
+                         <div class="datos w100 flex_x">
+                            <h6></h6>
+                            <h5 class="capi">${persona.patologia}</h5>
+                         </div>
+                         <div class="datos w100 flex_x">
+                             <h6>Ingreso</h6>
+                             <h6>${persona.ingreso}</h6>
+                        </div>
+                     </div>
+                     <div class="foot">
+                        <p class="material-icons-outlined">${persona.laboratorio}</p>
+                    </div>
                 </div>
-                <div class="datos w100 flex_x">
-                    <h6></h6>
-                    <h5 class="capi">${persona.patologia}</h5>
-                </div>
-                <div class="datos w100 flex_x">
-                    <h6>Ingreso</h6>
-                    <h6>${persona.ingreso}</h6>
-                </div>
-            </div>
-            <div class="foot">
-                <p class="material-icons-outlined">${persona.laboratorio}</p>
-            </div>
-        </div>
 
 
                 
